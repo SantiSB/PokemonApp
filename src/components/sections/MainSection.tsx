9c58d5e201872196ab1dff5c5a86b639c28a833c
SantiSB <a.santiago.salas.b@gmail.com>
2024-04-21 14:46:35 -0500

    search

--- src/components/sections/MainSection.tsx
+++ src/components/sections/MainSection.tsx
@@ -1,12 +1,10 @@
 'use client'
 import PokemonsList from '@/components/PokemonsList'
 import SearchInput from '../SearchInput'
-import { usePokemonList } from '@/hooks/usePokemonList'
 export default function MainSection() {
-  const { setFilter } = usePokemonList()
   return (
     <main className="bg-gray-50 dark:bg-gray-900 py-5">
-      <SearchInput onSearchChange={setFilter} />
+      <SearchInput />
       <PokemonsList />
     </main>
   )
