68f78e6a3604ca773772c52dad72fec304a20936
SantiSB <a.santiago.salas.b@gmail.com>
2024-04-22 23:08:12 -0500

    view transitions in all app

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-import Link from 'next/link'
+import { Link } from 'next-view-transitions'
 import { usePokemonContext } from '@/state/PokemonContext'
 import { Pokemon } from '@/types/pokemonTypes'
 import { usePokemonList } from '@/hooks/usePokemonList'
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-import Link from 'next/link'
+import { Link } from 'next-view-transitions'
 import { Press_Start_2P } from 'next/font/google'
 import { useAuth } from '@/hooks/useAuth'
 import { usePokemonList } from '@/hooks/usePokemonList'
