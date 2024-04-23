3f343942a52578ab350259077c47dc78431910ab
SantiSB <a.santiago.salas.b@gmail.com>
2024-04-22 22:37:21 -0500

    color change

--- src/app/pokemons/page.tsx
+++ src/app/pokemons/page.tsx
@@ -6,9 +6,11 @@ import SearchInput from '@/components/SearchInput'
 
 export default function Home() {
   const { loadFavorites } = usePokemonList()
+
   useEffect(() => {
     loadFavorites()
   }, [])
+
   return (
     <main className="bg-gray-50 dark:bg-gray-900 py-1">
       <SearchInput />
