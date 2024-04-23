16ae506ecdef05f8053a23744ca052e416487243
SantiSB <a.santiago.salas.b@gmail.com>
2024-04-23 12:20:07 -0500

    styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -60,7 +60,7 @@ export default function Login() {
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full text-white bg-primary-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-700 dark:hover:bg-primary-400 dark:focus:ring-primary-800"
               >
                 Sign in
               </button>
@@ -68,7 +68,7 @@ export default function Login() {
                 Don’t have an account yet?{' '}
                 <Link
                   href="/register"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  className="font-medium text-primary-700 hover:underline dark:text-primary-400"
                 >
                   Sign up
                 </Link>
